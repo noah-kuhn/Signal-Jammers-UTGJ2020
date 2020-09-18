ec9ae85f452bec1834f1a5f19c81c1f788f6969e
nk_ultra <kuhnnoah01@gmail.com>
2020-09-17 23:26:02 -0500

    Some Managers
    +GeneralManager abstract class
    +PlayerManager class extending GeneralManager, nothing inside yet
    +Managers folder to house them

---
+++ UTGJ2020/Assets/Scripts/Managers/GeneralManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class GeneralManager : MonoBehaviour
+{
+    //here's our property: now we can say GameManager.Instance to get the instance
+    //without worrying about making multiple GameManagers
+    public static GeneralManager Instance { get; private set; }
+
+    //make this the instance
+    void Awake() {
+        if (Instance == null)
+        {
+            Instance = this; //there is no GameManager-- so this can be it
+            DontDestroyOnLoad(gameObject); //pls don't destroy thanks
+        } else {
+            Destroy(gameObject); //ok there's already a GameManager. so die
+        }
+    }
+
+}
---
+++ UTGJ2020/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this class is empty and skeletal for now... but we'll populate it
+//with the relevant info when we have some more done!
+public class PlayerManager : GeneralManager
+{
+    
+}
