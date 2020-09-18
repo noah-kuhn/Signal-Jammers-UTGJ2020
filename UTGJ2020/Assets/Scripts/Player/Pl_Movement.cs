11d335728df4a0dc9b0f749aaa4a5996ed5736ae
nk_ultra <kuhnnoah01@gmail.com>
2020-09-18 14:55:11 -0500

    Even More Camera/Movement Tweaks
    ~More tweaks

--- UTGJ2020/Assets/Scripts/Player/Pl_Movement.cs
+++ UTGJ2020/Assets/Scripts/Player/Pl_Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pl_Movement : MonoBehaviour
 {
-    public float moveSpeed = 50f;
+    public float moveSpeed = 15f;
     [Tooltip("Changes the speed at which the player falls. Consequence of using CharacterController and not Rigidbody")]
     public float gravityScale = 3;
     private CharacterController controller;
@@ -16,7 +16,7 @@ public class Pl_Movement : MonoBehaviour
 
     private void FixedUpdate() {
         
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * moveSpeed;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized * moveSpeed;
         moveDirection.y += Physics.gravity.y * gravityScale;
         moveDirection = transform.TransformDirection(moveDirection);
         controller.Move(moveDirection * Time.deltaTime);
