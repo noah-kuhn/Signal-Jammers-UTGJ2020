5488aefbaa20f33db4ac617ea7c11543e983acc9
nk_ultra <kuhnnoah01@gmail.com>
2020-09-17 22:08:41 -0500

    Added BillBoard Script
    +BillBoard.cs

---
+++ UTGJ2020/Assets/Scripts/BillBoard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        transform.forward = new Vector3(Camera.main.transform.forward.x, transform.forward.y, Camera.main.transform.forward.z);
+    }
+}
