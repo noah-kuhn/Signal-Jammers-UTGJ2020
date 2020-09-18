641fc89c7f6a1c6297f3f9d55e999cb6a691c904
nk_ultra <kuhnnoah01@gmail.com>
2020-09-18 18:38:19 -0500

    The "Color Burst?" Update
    +Added ColorBurstTest scene
    +Added Colorer script (very bare bones for now)

---
+++ UTGJ2020/Assets/Scripts/Colorer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colorer : MonoBehaviour
+{
+    public ParticleSystem partSys;
+    public bool canBurst;
+
+    void Start(){
+        partSys = GetComponent<ParticleSystem>();
+        canBurst = false;
+    }
+
+    void Update(){
+        canBurst = !partSys.IsAlive();
+    }
+}
