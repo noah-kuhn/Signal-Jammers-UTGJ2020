3cea735b8925e95e58ae2bb2c20505df0e8ba5f6
gluethulhu <thexanbert@gmail.com>
2020-09-18 01:47:59 -0500

    Removed InputSystem
    InputSystem wasn't suiting our needs/I apparently can't make it work. I'm gonna use the built in input axes instead

--- UTGJ2020/Assets/Scripts/Player/Pl_Movement.cs
+++ UTGJ2020/Assets/Scripts/Player/Pl_Movement.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Pl_Movement : MonoBehaviour
 {
     private InputMaster controls;
-
+    private Rigidbody rb;
+    [Tooltip("Player speed")]
+    public float moveSpeed = 50f;
     private void Awake() {
         controls = new InputMaster();
+        rb = GetComponent<Rigidbody>();
         controls.Player.Movement.performed += context => Move(context.ReadValue<Vector2>());
     }
 
     void Move(Vector2 direction) {
         Debug.Log("Player wants to move " + direction);
+        rb.AddForce(direction * moveSpeed);
     }
 
     private void OnEnable() {
