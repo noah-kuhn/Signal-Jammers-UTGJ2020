a022b298cdf308573dce42eef2f250123056e326
Grant Ross <36680394+Grant-Ross@users.noreply.github.com>
2020-09-18 21:52:36 -0500

    Mailbox

--- UTGJ2020/Assets/Scripts/BillBoard.cs
+++ UTGJ2020/Assets/Scripts/BillBoard.cs
@@ -6,9 +6,32 @@ public class BillBoard : MonoBehaviour
 {
 
     public Camera cam;
+    public bool hasRotation;
+    private SpriteRenderer _spriteRenderer;
+    public Sprite frontSprite;
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite backSprite;
+    private float _baseRotation;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _baseRotation = transform.eulerAngles.y;
+    }
 
     private void Update()
     {
-        transform.forward = new Vector3(cam.transform.forward.x, transform.forward.y, cam.transform.forward.z);
+        transform.forward  = new Vector3(cam.transform.forward.x, transform.forward.y, cam.transform.forward.z);
+        
+        if (hasRotation)
+        {
+            var rotate = transform.eulerAngles.y-_baseRotation;
+            if (rotate < 0) rotate += 360;
+            if (rotate > 180) rotate -= 360;
+            
+            _spriteRenderer.sprite = rotate < 45 && rotate > -45 ? frontSprite :
+                rotate >= 45 && rotate <= 135 ? leftSprite :
+                rotate <= -45 && rotate >= -135 ? rightSprite : backSprite;
+        }
     }
 }
