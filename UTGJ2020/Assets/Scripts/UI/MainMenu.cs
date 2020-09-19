7e92bf0b92aad9a93c27c9a0f75a24ca202ae1cd
gluethulhu <thexanbert@gmail.com>
2020-09-18 19:57:39 -0500

    Main Menu + TextMeshPro
    Created a VERY basic main menu
    Added a fade in fade out script/animation
    Imported TextMeshPro

---
+++ UTGJ2020/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Tooltip("Speed at which the screen will fade out when going to next scene. 1 is normal speed")]
+    public float fadeOutSpeed = 1f;
+    public Image fadeImage;
+    private Fade fadeUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeUI = FindObjectOfType<Fade>();
+        fadeImage = fadeUI.gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RunFirstScene() {
+        StartCoroutine(FadeOut("MoveTest"));
+    }
+
+    IEnumerator FadeOut(string scene) {
+        fadeUI.FadeOut(fadeOutSpeed);
+        yield return new WaitUntil(() => fadeImage.color.a == 1);
+        SceneManager.LoadScene(scene);
+    }
+}
