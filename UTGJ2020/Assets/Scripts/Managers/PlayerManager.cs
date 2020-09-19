4840ab4c5588dd942da50cb3fa68078ec6a0c36b
nk_ultra <kuhnnoah01@gmail.com>
2020-09-18 21:11:55 -0500

    The Managerial Update
    -Removed GeneralManager
    +Added some yummy stuff to PlayerManager
    ~Minor stuff?

--- UTGJ2020/Assets/Scripts/Managers/PlayerManager.cs
+++ UTGJ2020/Assets/Scripts/Managers/PlayerManager.cs
@@ -1,10 +1,28 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//this class is empty and skeletal for now... but we'll populate it
-//with the relevant info when we have some more done!
-public class PlayerManager : GeneralManager
+public class PlayerManager : MonoBehaviour
 {
-    
+    //here's our property: now we can say PlayerManager.Instance to get the instance
+    //without worrying about making multiple PlayerManagers
+    public static PlayerManager Instance { get; private set; }
+
+    //make this the instance
+    void Awake() {
+        if (Instance == null)
+        {
+            Instance = this; //there is no PlayerManager-- so this can be it
+            DontDestroyOnLoad(gameObject); //pls don't destroy thanks
+        } else {
+            Destroy(gameObject); //ok there's already a PlayerManager. so die
+        }
+    }
+
+    //okay let's list the data we need to transfer between scenes!
+    public static ColorIDs.Colors currCol{ get; set; }
+    public static List<ColorIDs.Colors> availableColors{ get; set; }
+    public static Camera mainCamera{ get; set; }
+    public static GameObject player{ get; set; }
+
 }
