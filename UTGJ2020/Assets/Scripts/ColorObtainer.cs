32c3f8616eb41797cdc2834d9816722d98dd4138
gluethulhu <thexanbert@gmail.com>
2020-09-19 02:24:36 -0500

    Added Color obtainer script

---
+++ UTGJ2020/Assets/Scripts/ColorObtainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorObtainer : MonoBehaviour
+{
+    [Tooltip("Color to be obtained when this item is picked up")]
+    public ColorIDs.Colors color;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("Player")) 
+        {
+            PlayerManager.AvailableColors.Add(color);
+        }
+    }
+
+    //TODO: When color is obtained, add color UI element to pallete UI. 
+    //Give tooltip on how to use colors/switch colors for first and second respectively?
+
+}
