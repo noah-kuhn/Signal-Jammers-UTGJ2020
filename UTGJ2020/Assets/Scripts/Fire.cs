8467c67d20cd74b5be55c028358d7a60b4624856
Grant Ross <36680394+Grant-Ross@users.noreply.github.com>
2020-09-20 10:46:51 -0500

    fire

---
+++ UTGJ2020/Assets/Scripts/Fire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private GameObject front;
+    private GameObject back;
+    private Animator frontanim;
+    private Animator backanim;
+    public BoxCollider death;
+    public bool active;
+
+    private void Start()
+    {
+        front = transform.GetChild(0).gameObject;
+        back = transform.GetChild(1).gameObject;
+        frontanim = front.GetComponent<Animator>();
+        backanim = back.GetComponent<Animator>();
+        death = front.GetComponent<BoxCollider>();
+        death.enabled = active;
+        frontanim.SetBool("active",active);
+        backanim.SetBool("active",active);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Burst")&& PlayerManager.CurrentColor == ColorIDs.Colors.Red)
+        {
+            active = !active;
+            death.enabled = active;
+            frontanim.SetBool("active",active);
+            backanim.SetBool("active",active);
+        }
+    }
+}
