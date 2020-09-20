9eecc3198eb35cb0e4215e3775c1e34dfa535f1e
gluethulhu <thexanbert@gmail.com>
2020-09-20 09:38:24 -0500

    Added mouse click indicators
    Mouse click indicators in scene 1

---
+++ UTGJ2020/Assets/Scripts/MouseIcon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseIcon : MonoBehaviour
+{
+    public SpriteRenderer sRenderer;
+    // Start is called before the first frame update
+    private void Start() {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sRenderer.enabled = false;        
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            sRenderer.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            sRenderer.enabled = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
