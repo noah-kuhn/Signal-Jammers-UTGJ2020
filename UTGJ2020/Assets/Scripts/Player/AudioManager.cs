210813be7e0468d60bc80942059ee526e6f07e82
Grant Ross <36680394+Grant-Ross@users.noreply.github.com>
2020-09-19 20:12:39 -0500

    audio stuff

---
+++ UTGJ2020/Assets/Scripts/Player/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public enum SoundIDs
+    {
+        MAIN_MUSIC,
+        BURST,
+        WALK,
+        LAND
+        
+    }
+
+    public float masterVolume;
+    public Sound[] sounds;
+    public AudioSource musicSource;
+
+    
+
+    private void Awake()
+    {
+        foreach (var s in sounds)
+        {
+            if (s.Id != SoundIDs.MAIN_MUSIC)
+            {
+                s.Source = gameObject.AddComponent<AudioSource>();
+                s.Source.clip = s.Clip;
+                s.Source.volume = s.Volume;
+                s.Source.loop = s.Loop;
+            }
+        }
+        musicSource.loop = true;
+    }
+
+    public void PlayMusic(SoundIDs id)
+    {
+        foreach (var s in sounds)
+        {
+            if (s.Id == id)
+            {
+                musicSource.clip = s.Clip;
+                musicSource.volume = s.Volume;
+            }
+        }
+    }
+    
+    public void PlaySound(SoundIDs id)
+    {
+        foreach (var s in sounds)
+        {
+            if (s.Id == id)
+            {
+                s.Source.Play();
+                break;
+            }
+        }
+    }
+    
+}
---
+++ UTGJ2020/Assets/Scripts/Player/Sound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+    // Start is called before the first frame update
+    public AudioClip Clip;
+    [Range(0f,1f)]
+    public float Volume;
+    public bool Loop;
+    public AudioManager.SoundIDs Id;
+    [HideInInspector]
+    public AudioSource Source;
+}
